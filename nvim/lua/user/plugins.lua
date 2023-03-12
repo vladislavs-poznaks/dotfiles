ab24f853d4d3a2257d1db8b7a858c622d87e5d56
Vladislavs Pozņaks <vladislavs.poznaks@gmail.com>
2023-03-12 15:28:39 +0200

    Changed keybindings for floaterm

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -145,8 +145,8 @@ use({
   config = function()
     vim.g.floaterm_width = 0.8
     vim.g.floaterm_height = 0.8
-    vim.keymap.set('n', '<leader>tt', ':FloatermToggle<CR>')
-    vim.keymap.set('t', '<leader>tt', '<C-\\><C-n>:FloatermToggle<CR>')
+    vim.keymap.set('n', 'TT', ':FloatermToggle<CR>')
+    vim.keymap.set('t', 'TT', '<C-\\><C-n>:FloatermToggle<CR>')
     vim.cmd([[
       highlight link Floaterm CursorLine
       highlight link FloatermBorder CursorLineBg
